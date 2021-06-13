43218daabd7c727d845e6cf82b980e047921395f
Jordan Santell <jordan@jsantell.com>
2021-06-13 13:56:57 -0700

    Add build script

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
+rm -rf dist
 rm -rf v8-api-search
 mkdir v8-api-search
+mkdir dist
+version=$(cat manifest.json | jq .version | tr -d \")
+output_name="v8_api_search-${version}";
 
-cp -r src v8-api-search
-cp -r web_modules v8-api-search
+echo "Generating $output_name"
+
 cp -r assets v8-api-search
-cp package.json v8-api-search
+cp -r src v8-api-search
+cp -r data v8-api-search
 cp manifest.json v8-api-search
 cp README.md v8-api-search
 cp LICENSE v8-api-search
 
-if [ ! -z "$1" ] && [ $1 == 'chrome' ] ;
-then
-  echo "Building Chrome variation."
-  cat manifest.json | \
-    json -e 'delete this.browser_specific_settings' > \
-    v8-api-search/manifest.json
-fi
-
-npx web-ext build \
+# Build Firefox
+web-ext build \
   --source-dir v8-api-search \
-  --artifacts-dir dist \
-  --overwrite-dest
+  --artifacts-dir dist
+mv "dist/$output_name.zip" "dist/$output_name-firefox.zip"
 
-rm -r v8-api-search
+# Build Chrome 
+cat manifest.json | \
+  jq 'del(.browser_specific_settings)' > \
+  v8-api-search/manifest.json
+web-ext build \
+  --source-dir v8-api-search \
+  --artifacts-dir dist
+mv "dist/$output_name.zip" "dist/$output_name-chrome.zip"
+
+rm -rf v8-api-search
